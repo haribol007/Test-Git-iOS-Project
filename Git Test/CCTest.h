038f31ac0399654af6ad441d2364159e5cd08583
Pravin <pravinkerai@Pravins-MacBook-Pro.local>
2015-02-18 16:38:06 +0530

    added test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Pravin on 18/02/15.
+//  Copyright (c) 2015 haribol devs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Pravin on 18/02/15.
+//  Copyright (c) 2015 haribol devs. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
