41bdaedffbbc10ab0a24f45deb6989bef2162a13
Pravin <pravinkerai@Pravins-MacBook-Pro.local>
2015-02-18 16:16:16 +0530

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Pravin on 18/02/15.
+//  Copyright (c) 2015 haribol devs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Pravin on 18/02/15.
+//  Copyright (c) 2015 haribol devs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
